32bfc18e7bb3e2057a17d8c57c2f47705745e6df
keshav <keshavraj4546@gmail.com>
2023-04-28 22:21:08 +0530

    Print log on tap of button

--- app/src/main/java/com/example/constraintslayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintslayout/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.constraintslayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.country
+import kotlinx.android.synthetic.main.activity_main.dob
+import kotlinx.android.synthetic.main.activity_main.firstName
+import kotlinx.android.synthetic.main.activity_main.lastName
+import kotlinx.android.synthetic.main.activity_main.nextButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // In java
+//      val buttonApply = findViewById<Button>(R.id.nextButton)
+
+        // In Kotlin
+        nextButton.setOnClickListener {
+            val firstName = firstName.text.toString()
+            val lastName = lastName.text.toString()
+            val dob = dob.text.toString()
+            val country = country.text.toString()
+           Log.d("MainActivity","$firstName $lastName, born on $dob, country $country")
+        }
+
     }
 }
